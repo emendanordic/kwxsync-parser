deab0e0115e42127652c9e5e488e82fa7a93ae22
benemenda <ben.marsden@emenda.se>
2019-03-12 08:47:10 +0200

    Added -s, --sleep feature: appends the provided sleep command to every kwxsync command.

--- kwxsync_parser.py
+++ kwxsync_parser.py
@@ -29,11 +29,13 @@ else:
 g_version = 1.0
 g_projects = ""
 g_rest = ""
+g_sleep = False
 
 parser = argparse.ArgumentParser(prog=os.path.basename(__file__),
                                  description="returns several kwxsync commands for a given kwxsync command")
-parser.add_argument('-c', "--command", nargs='?', required=True, metavar="all kwxsync arguments", help="kwxsync command arguments, e.g. '""""--statuses "Analyze","Ignore","Not a problem","Fix","Fix in Next Release","Defer","Filter" --last-sync "05-03-2016 00:00:00" --host segotl3729.got.volvo.net --port 8080'""")
+parser.add_argument('-c', "--command", nargs='?', required=True, metavar="all kwxsync arguments", help="kwxsync command arguments, e.g. '""""--statuses "Analyze","Ignore","Not a problem","Fix","Fix in Next Release","Defer","Filter" --last-sync "05-03-2016 00:00:00" --host test.net --port 8080'""")
 parser.add_argument('-p', "--projects", nargs='?', required=True, metavar="list of projects", help="lists of projects with which to perform kwxsync across")
+parser.add_argument('-s', "--sleep", nargs='?', required=False, metavar="sleep command", help="provide sleep command if you wish to pause between each call to kwxsync, e.g. 'sleep(2)'")
 
 # Message to be displayed on start
 def start_message(filename, version, args=None):
@@ -60,17 +62,34 @@ def construct_kwxsync_commands(project_tuples, g_rest):
         commandList.append(command)
     for command in commandList:
         print(command)
-    return False
+        if g_sleep:
+            print("sleep(2)")
+    return commandList
 
+# Open file
+def open_file(name, x):
+    try:
+        f = open(name, x)
+    except IOError as e:
+        output("ERROR: %s" % e, level.ERROR)
+        end_message(1)
+    return f
 
+def writeOutputToFile(f, commandList):
+    for command in commandList:
+        f.write(command + "\n")
+        if g_sleep:
+            f.write(g_sleep+ "\n")
 def main():
-    global g_rest, g_projects
+    global g_rest, g_projects, g_sleep
     args = parser.parse_args()
     g_rest = args.command
     g_projects = args.projects
+    g_sleep = args.sleep
     start_message(os.path.basename(__file__), g_version, {'projects': g_projects, 'rest': g_rest})
     project_tuples = create_project_tuples(g_projects)
-    construct_kwxsync_commands(project_tuples, g_rest)
-
+    commandList = construct_kwxsync_commands(project_tuples, g_rest)
+    f = open_file("kwxsync_commands.out", "w+")
+    writeOutputToFile(f, commandList)
 if __name__ == '__main__':
     main()
