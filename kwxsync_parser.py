79352302bcc0b3146e817ef0d3259d5c08916be1
benemenda <ben.marsden@emenda.se>
2019-03-15 15:34:01 +0200

    Added progress indicator with -i, --indicator argument.

--- kwxsync_parser.py
+++ kwxsync_parser.py
@@ -30,12 +30,14 @@ g_version = 1.0
 g_projects = ""
 g_commands = ""
 g_sleep = ""
+g_indicator = False
 
 parser = argparse.ArgumentParser(prog=os.path.basename(__file__),
                                  description="returns several kwxsync commands for a given kwxsync command")
 parser.add_argument('-c', "--command", nargs='?', required=True, metavar="all kwxsync arguments", help="kwxsync command arguments, e.g. '""""--statuses "Analyze","Ignore","Not a problem","Fix","Fix in Next Release","Defer","Filter" --last-sync "05-03-2016 00:00:00" --host test.net --port 8080'""")
 parser.add_argument('-p', "--projects", nargs='?', required=True, metavar="list of projects", help="lists of projects with which to perform kwxsync across")
 parser.add_argument('-s', "--sleep", nargs='?', required=False, metavar="sleep command", help="provide sleep command if you wish to pause between each call to kwxsync, e.g. 'sleep(2)'")
+parser.add_argument('-i', "--indicator", action="store_true", help="use if you wish to receive an indication of progress")
 
 # Message to be displayed on start
 def start_message(filename, version, args=None):
@@ -72,11 +74,16 @@ def open_file(name, x):
     return f
 
 def writeOutputToFile(f, commandList):
+    progressInt = 1
+    numberCommands = len(commandList)
     for command in commandList:
         f.write(command + "\n")
         if g_sleep:
             f.write(g_sleep+ "\n")
-
+        if g_indicator:
+            progressNotification="echo %s/%s kwxsync commands executed." %(progressInt, numberCommands)
+            f.write(progressNotification + "\n")
+            progressInt=progressInt+1
 # Message to be displayed at end of execution
 def end_message(code):
     if code == 0:
@@ -91,11 +98,12 @@ def end_message(code):
     sys.exit(code)
 
 def main():
-    global g_commands, g_projects, g_sleep
+    global g_commands, g_projects, g_sleep, g_indicator
     args = parser.parse_args()
     g_commands = args.command
     g_projects = args.projects
     g_sleep = args.sleep
+    g_indicator = args.indicator
     start_message(os.path.basename(__file__), g_version, {'projects': g_projects, 'command': g_commands})
     project_tuples = create_project_tuples(g_projects)
     commandList = construct_kwxsync_commands(project_tuples, g_commands)
